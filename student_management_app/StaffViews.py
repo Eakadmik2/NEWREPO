a9a7fcec087c556153ffb8dd497043e5fd8e486d
Prateek Singh <s.prateek3801@gmail.com>
2021-04-21 14:26:52 +0530

    Update Attendance, Staff Leave Apply and Send Feedback Done

--- student_management_app/StaffViews.py
+++ student_management_app/StaffViews.py
@@ -1,50 +1,181 @@
+import json
+from datetime import datetime
+from uuid import uuid4
+
+from django.contrib import messages
+from django.core import serializers
+from django.forms import model_to_dict
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
+from student_management_app.models import Subjects, SessionYearModel, Students, Attendance, AttendanceReport, \
+    LeaveReportStaff, Staffs, FeedBackStaffs, CustomUser, Courses, NotificationStaffs, StudentResult, OnlineClassRoom
 
 
 def staff_home(request):
     return render(request, "staff_template/staff_home_template.html")
 
+
 def staff_take_attendance(request):
-    subjects=Subjects.objects.filter(staff_id=request.user.id)
-    session_year=SessionYearModel.object.all()
-    return render(request,"staff_template/staff_take_attendance.html",{"subjects":subjects,"session_years":session_year})
+    subjects = Subjects.objects.filter(staff_id=request.user.id)
+    session_year = SessionYearModel.object.all()
+    return render(request, "staff_template/staff_take_attendance.html", {"subjects": subjects, "session_years": session_year})
+
 
 @csrf_exempt
 def get_students(request):
-    subject_id=request.POST.get("subject")
-    session_year=request.POST.get("session_year")
+    subject_id = request.POST.get("subject")
+    session_year = request.POST.get("session_year")
 
-    subject=Subjects.objects.get(id=subject_id)
-    session_model=SessionYearModel.object.get(id=session_year)
-    students=Students.objects.filter(course_id=subject.course_id,session_year_id=session_model)
-    student_data=serializers.serialize("python",students)
-    list_data=[]
+    subject = Subjects.objects.get(id=subject_id)
+    session_model = SessionYearModel.object.get(id=session_year)
+    students = Students.objects.filter(
+        course_id=subject.course_id, session_year_id=session_model)
+    student_data = serializers.serialize("python", students)
+    list_data = []
 
     for student in students:
-        data_small={"id":student.admin.id,"name":student.admin.first_name+" "+student.admin.last_name}
+        data_small = {"id": student.admin.id,
+                      "name": student.admin.first_name+" "+student.admin.last_name}
         list_data.append(data_small)
 
-    return JsonResponse(json.dumps(list_data),content_type="application/json",safe=False)
+    return JsonResponse(json.dumps(list_data), content_type="application/json", safe=False)
+
 
 @csrf_exempt
 def save_attendace_data(request):
-    student_ids=request.POST.get("student_ids")
-    student_id=request.POST.get("student_id")
-    attendance_date=request.POST.get("attendance_date")
-    session_year_id=request.POST.get("session_year_id")
+    student_ids = request.POST.get("student_ids")
+    student_id = request.POST.get("student_id")
+    attendance_date = request.POST.get("attendance_date")
+    session_year_id = request.POST.get("session_year_id")
 
-    subject_model=Subjects.objects.get(id=subject_id)
-    session_model=SessionYearModel.object.get(id=session_year_id)
-    json_sstudent=json.loads(student_ids)
+    subject_model = Subjects.objects.get(id=subject_id)
+    session_model = SessionYearModel.object.get(id=session_year_id)
+    json_sstudent = json.loads(student_ids)
 
     try:
-        attendace=Attendance(subject_id=subject_model,attendance_date=attendance_date,session_year_id=session_model)
+        attendace = Attendance(
+            subject_id=subject_model, attendance_date=attendance_date, session_year_id=session_model)
         attendace.save()
 
         for stud in json_sstudent:
-            student=Students.object.get(admin=stud['id'])
-            attendace_report=AttendanceReport(student_id=student,attendace_id=attendace,status=stud['status'])
+            student = Students.object.get(admin=stud['id'])
+            attendace_report = AttendanceReport(
+                student_id=student, attendace_id=attendace, status=stud['status'])
             attendace.report.save()
         return HttpResponse("OK")
     except:
         return HttpResponse("Error")
+
+
+def staff_update_attendance(request):
+    subjects = Subjects.objects.filter(staff_id=request.user.id)
+    session_year_id = SessionYearModel.object.all()
+    return render(request, "staff_template/staff_update_attendance.html", {"subjects": subjects})
+
+
+@csrf_exempt
+def get_attendance_dates(request):
+    subject = request.POST.get("subject")
+    session_year_id = request.POST.get("session_year_id")
+    subject_obj = Subjects.objects.get(id=subject)
+    session_year_obj = SessionYearModel.object.get(id=session_year_id)
+    attendance = Attendance.objects.filter(
+        subject_id=subject_obj, session_year_id=session_year_obj)
+    attendance_obj = []
+    for attendance_single in attendance:
+        data = {"id": attendance_single.id, "attendance_date": str(
+            attendance_single.attendance_date), "session_year_id": attendance_single.session_year_id.id}
+        attendance_obj.append(data)
+
+    return JsonResponse(json.dumps(attendance_obj), safe=False)
+
+
+@csrf_exempt
+def get_attendance_student(request):
+    attendance_date = request.POST.get("attendance_date")
+
+    attendance = Attendance.objects.get(id=attendance_date)
+
+    attendance_data = AttendanceReport.objects.filter(attendance_id=attendance)
+    students = Students.objects.filter(
+        course_id=subject.course_id, session_year_id=session_model)
+    list_data = []
+
+    for student in students:
+        data_small = {"id": student.student_id.admin.id,
+                      "name": student.student_id.admin.first_name+" "+student.student_id.admin.last_name, "status": student.status}
+        list_data.append(data_small)
+
+    return JsonResponse(json.dumps(list_data), content_type="application/json", safe=False)
+
+
+@csrf_exempt
+def save_updateattendance_data(request):
+    student_ids = request.POST.get("student_ids")
+    attendance_date = request.POST.get("attendance_date")
+    attendance = Attendance.objects.get(id=attendance_date)
+
+    json_sstudent = json.loads(student_ids)
+
+    try:
+        for stud in json_sstudent:
+            student = Students.object.get(admin=stud['id'])
+            attendance_report = AttendanceReport.objects.get(
+                student_id=student, attendace_id=attendance)
+            attendance.report.status = stud['status']
+            attendance.report.save()
+        return HttpResponse("OK")
+    except:
+        return HttpResponse("Error")
+
+
+def staff_apply_leave(request):
+    staff_obj = Staffs.objects.get(admin=request.user.id)
+    leave_data = LeaveReportStaff.objects.filter(staff_id=staff_obj)
+    return render(request, "staff_template/staff_apply_leave.html", {"leave_data": leave_data})
+
+
+def staff_apply_leave_save(request):
+    if request.method != "POST":
+        return HttpResponseRedirect(reverse("staff_apply_leave"))
+    else:
+        leave_date = request.POST.get("leave_date")
+        leave_msg = request.POST.get("leave_msg")
+
+        staff_obj = Staffs.objects.get(admin=request.user.id)
+        try:
+            leave_report = LeaveReportStaff(
+                staff_id=staff_obj, leave_date=leave_date, leave_message=leave_msg, leave_status=0)
+            leave_report.save()
+            messages.success(request, "Successfully Applied for Leave")
+            return HttpResponseRedirect(reverse("staff_apply_leave"))
+        except:
+            messages.error(request, "Failed To Apply for Leave")
+            return HttpResponseRedirect(reverse("staff_apply_leave"))
+
+
+def staff_feedback(request):
+    staff_id = Staffs.objects.get(admin=request.user.id)
+    feedback_data = FeedBackStaffs.objects.filter(staff_id=staff_id)
+    return render(request, "staff_template/staff_feedback.html", {"feedback_data": feedback_data})
+
+
+def staff_feedback_save(request):
+    if request.method != "POST":
+        return HttpResponseRedirect(reverse("staff_feedback_save"))
+    else:
+        feedback_msg = request.POST.get("feedback_msg")
+
+        staff_obj = Staffs.objects.get(admin=request.user.id)
+        try:
+            feedback = FeedBackStaffs(
+                staff_id=staff_obj, feedback=feedback_msg, feedback_reply="")
+            feedback.save()
+            messages.success(request, "Successfully Sent Feedback")
+            return HttpResponseRedirect(reverse("staff_apply_leave"))
+        except:
+            messages.error(request, "Failed To Send Feedback")
+            return HttpResponseRedirect(reverse("staff_feedback"))
--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -59,12 +59,31 @@ urlpatterns = [
     path('staff_home', StaffViews.staff_home, name="staff_home"),
     path('student_home', StudentViews.student_home, name="student_home"),
 
-    path('manage_session',HodViews.manage_session,name="manage_session"),
-    path('add_session_save',HodViews.add_session_save,name="add_session_save"),
+    path('manage_session', HodViews.manage_session, name="manage_session"),
+    path('add_session_save', HodViews.add_session_save, name="add_session_save"),
 
-    path('staff_take_attendance',StaffViews.staff_take_attendance,name="staff_take_attendance"),
-    path('get_student',StaffViews.get_students,name="get_students"),
-    path('save_attendace_data',StaffViews.save_attendace_data,name="save_attendace_data"),
+    path('staff_take_attendance', StaffViews.staff_take_attendance,
+         name="staff_take_attendance"),
+    path('staff_update_attendance', StaffViews.staff_update_attendance,
+         name="staff_update_attendance"),
+    path('get_students', StaffViews.get_students, name="get_students"),
+    path('get_attendance_dates', StaffViews.get_attendance_dates,
+         name="get_attendance_dates"),
+    path('get_attendance_student', StaffViews.get_attendance_student,
+         name="get_attendance_student"),
+    path('save_attendace_data', StaffViews.save_attendace_data,
+         name="save_attendace_data"),
+    path('save_updateattendance_data', StaffViews.save_updateattendance_data,
+         name="save_updateattendance_data"),
+    path('staff_apply_leave', StaffViews.staff_apply_leave,
+         name="staff_apply_leave"),
+    path('staff_apply_leave_save', StaffViews.staff_apply_leave_save,
+         name="staff_apply_leave_save"),
+    path('staff_feedback', StaffViews.staff_feedback,
+         name="staff_feedback"),
+    path('staff_feedback_save', StaffViews.staff_feedback_save,
+         name="staff_feedback_save"),
+
 
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
