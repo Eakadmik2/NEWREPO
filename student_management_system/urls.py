7e2e5f8e4dec3c0074265027afb5a9da440f6faf
Shishir Parakh <shishirparakh@gmail.com>
2021-04-22 15:17:17 +0530

    Added Edit Profile for Staff, Admin and Student

--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -75,6 +75,8 @@ urlpatterns = [
     path('admin_view_attendance', HodViews.admin_view_attendance, name="admin_view_attendance"),
     path('admin_get_attendance_dates', HodViews.admin_get_attendance_dates, name="admin_get_attendance_dates"),
     path('admin_get_attendance_student', HodViews.admin_get_attendance_student, name="admin_get_attendance_student"),
+    path('admin_profile', HodViews.admin_profile, name="admin_profile"),
+    path('admin_profile_save', HodViews.admin_profile_save, name="admin_profile_save"),
 
     path('staff_take_attendance', StaffViews.staff_take_attendance, name="staff_take_attendance"),
     path('staff_update_attendance', StaffViews.staff_update_attendance,name="staff_update_attendance"),
@@ -87,13 +89,17 @@ urlpatterns = [
     path('staff_apply_leave_save', StaffViews.staff_apply_leave_save,name="staff_apply_leave_save"),
     path('staff_feedback', StaffViews.staff_feedback,name="staff_feedback"),
     path('staff_feedback_save', StaffViews.staff_feedback_save,name="staff_feedback_save"),
+    path('staff_profile', StaffViews.staff_profile, name="staff_profile"),
+    path('staff_profile_save', StaffViews.staff_profile_save, name="staff_profile_save"),
+
     path('student_view_attendance', StudentViews.student_view_attendance,name="student_view_attendance"),
     path('student_view_attendance_post', StudentViews.student_view_attendance_post,name="student_view_attendance_post"),
     path('student_apply_leave', StudentViews.student_apply_leave,name="student_apply_leave"),
     path('student_apply_leave_save', StudentViews.student_apply_leave_save,name="student_apply_leave_save"),
     path('student_feedback', StudentViews.student_feedback, name="student_feedback"),
     path('student_feedback_save', StudentViews.student_feedback_save,name="student_feedback_save"),
-
+    path('student_profile', StudentViews.student_profile, name="student_profile"),
+    path('student_profile_save', StudentViews.student_profile_save, name="student_profile_save"),
 
 
 
